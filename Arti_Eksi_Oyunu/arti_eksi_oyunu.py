f5fe35d7d73329bbb7444cbc2e553ba16ad09905
Necmettin Hamza Gürbüz <necmettinhamzagurbuzz@gmail.com>
2023-07-22 17:42:10 +0300

    Update arti_eksi_oyunu.py

--- Arti_Eksi_Oyunu/arti_eksi_oyunu.py
+++ Arti_Eksi_Oyunu/arti_eksi_oyunu.py
@@ -1,36 +1,45 @@
 import time
 import random
 
-list = []
-
+liste = []
 for i in range(1, 101):
-    list.append(i)
+    liste.append(i)
 
-rastgele = random.choice(list)
+rastgele = random.choice(liste)
 
-
-print("Merhaba bugün seninle bir oyun oynayacağız")
+print("Merhaba! Bugün seninle bir oyun oynayacağız.")
+time.sleep(3)
+print("Ben 1'den 100'e kadar bir sayı tutacağım ve sen tahmin etmeye çalışacaksın.")
+time.sleep(3)
+print("Eğer sana '+' dersem bil ki sayı daha yüksek, eğer sana '-' dersem bil ki sayı daha düşük.")
+time.sleep(3)
+print("Tamamdır! Hadi başlayalım.")
 time.sleep(2)
-print("Ben 1'den 100 e kadar bir sayı tutacağım ve sen tahmin etmeye çalışacaksın")
-time.sleep(2)
-print("Eğer sana '+' dersem bil ki sayı daha yüksek eğer sana '-' dersem bilki sayı daha düşük.")
-time.sleep(2)
-print("Hadi Başlayalım")
 
+deneme_hakki = int(input("Kaç deneme hakkı istersin?: "))
 
-
-print("Ben sayımı tuttum")
-
-
-while True:
-    tahmin = int(input("Tahminin nedir:"))
-    if tahmin > rastgele:
-        print("Bu Yanlış : -")
+while deneme_hakki > 0:
+    tahmin = int(input("Tahminin nedir: "))
+    
+    if not 1 <= tahmin <= 100:
+        print("Lütfen belirtilen aralıkta bir sayı giriniz.")
         continue
+
+    if tahmin > rastgele:
+        print("Bu yanlış: -")
+        time.sleep(0.4)
+        
     elif tahmin < rastgele:
         print("Bu yanlış: +")
-        continue
-    else:
-        print("Tebrikler!!", rastgele,"Sayısını tutmuştum")
-        break
+        time.sleep(0.4)
         
+    else:
+        print("Tebrikler!!", rastgele, "Sayısını tutmuştum.")
+        break
+
+    deneme_hakki -= 1
+
+    if deneme_hakki == 0:
+        print("Maalesef hakkın doldu. Doğru sayı:", rastgele)
+
+time.sleep(5)
